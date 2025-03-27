66e7d04c1c3330b4964b0bae68c9b6ec0446b7f0
Jesus Salcedo <jesalceg@gmail.com>
2025-03-27 17:54:52 -0500

    feature: add base functions to generate

---
+++ functions.py
@@ -0,0 +1,21 @@
+import qrcode
+import qrcode.constants
+
+class Functions:
+
+    def generate(text: str, filename: str = 'qr_code.png'):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4
+        )
+        qr.add_data(text)
+        qr.make(fit=True)
+
+        image = qr.make_image(fill_color='black', back_color='white')
+        image.save(filename)
+        print(f'QR code generate as {filename}')
+
+        
+
