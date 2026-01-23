e2039ab5aa58d6e6a703f660a900634f573c254d
Jesus Salcedo <jesalceg@gmail.com>
2026-01-22 20:39:43 -0500

    fix: update environtment checkig for CI

--- wait_for_db.py
+++ wait_for_db.py
@@ -12,19 +12,20 @@ def wait_for_postgres():
     db_user = os.getenv("POSTGRES_USER")
     db_pass = os.getenv("POSTGRES_PASSWORD")
 
-    for i in range(max_retries):
-        try:
-            conn = psycopg2.connect(
-                host=db_host,
-                port=db_port,
-                dbname=db_name,
-                user=db_user,
-                password=db_pass
-            )
-            conn.close()
-            print("✅ Postgres is ready!")
-            return
-        except psycopg2.OperationalError as e:
-            print(f"⏳ Waiting for Postgres ({i+1}/{max_retries})... {e}")
-            time.sleep(wait_seconds)
-    raise Exception("❌ Could not connect to Postgres after several retries.")
+    if os.getenv("ENV") != "ci":
+        for i in range(max_retries):
+            try:
+                conn = psycopg2.connect(
+                    host=db_host,
+                    port=db_port,
+                    dbname=db_name,
+                    user=db_user,
+                    password=db_pass
+                )
+                conn.close()
+                print("✅ Postgres is ready!")
+                return
+            except psycopg2.OperationalError as e:
+                print(f"⏳ Waiting for Postgres ({i+1}/{max_retries})... {e}")
+                time.sleep(wait_seconds)
+        raise Exception("❌ Could not connect to Postgres after several retries.")
