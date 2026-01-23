263101044b0f96e75108a219d091f922b7d69215
Jesus Salcedo <jesalceg@gmail.com>
2026-01-22 21:22:57 -0500

    refactor: adjust main.py

--- app/main.py
+++ app/main.py
@@ -1,31 +1,20 @@
-
 import os
-from app.middlewares.logging_middleware import LoggingMiddleware
-from app.models.user import Base
-from app.core.database import engine
-from app.routes import private, users, auth, posts, categories, content_types
 from fastapi import FastAPI
+from app.middlewares.logging_middleware import LoggingMiddleware
+from app.routes import private, users, auth, posts, categories, content_types
 from app.routes.dynamic import load_dynamic_routers
-from wait_for_db import wait_for_postgres
 
-# helper for wait for postgress connection
-if os.getenv("SKIP_DB_WAIT") != "true":
-    from wait_for_db import wait_for_postgres
-    wait_for_postgres()
+app = FastAPI(
+    swagger_ui_parameters={"syntaxHighlight": False},
+    title="Epikra",
+    description="Headless, powerful and poisonless CMS API.",
+    version="1.0.0",
+)
 
-# Create database table xd
-Base.metadata.create_all(bind=engine)
-
-# documentation hehe
-app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False},
-              title="Epikra",
-              description="Headless, powerful and poisonless CMS API.",
-              version="1.0.0")
-
-# Middleware for logging
+# Middleware
 app.add_middleware(LoggingMiddleware)
 
-# basic routing
+# Routes
 app.include_router(users.router)
 app.include_router(private.router)
 app.include_router(auth.router)
@@ -34,3 +23,19 @@ app.include_router(categories.router)
 app.include_router(content_types.router, prefix="/content-types")
 
 load_dynamic_routers(app)
+
+
+# Startup lifecycle
+@app.on_event("startup")
+def startup_event():
+    if os.getenv("SKIP_DB_WAIT") == "true":
+        print("🟡 SKIP_DB_WAIT=true → DB initialization skipped")
+        return
+
+    from wait_for_db import wait_for_postgres
+    from app.core.database import engine
+    from app.models.user import Base
+
+    wait_for_postgres()
+    Base.metadata.create_all(bind=engine)
+    print("✅ Database ready")
